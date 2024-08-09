023fc82b004eddddb22f03fd02239013ebbb04ec
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-08-10 02:25:11 +0300

    jhgjf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,56 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { nanoid } from 'nanoid';
-import ContactList from './ContactList/ContactList';
-import SearchBox from './SearchBox/SearchBox';
-import ContactForm from './ContactForm/ContactForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { nanoid } from "nanoid";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactForm from "./ContactForm/ContactForm";
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
+  // const [contacts, setContacts] = useState(() => {
+  //   const savedContacts = localStorage.getItem('contacts');
 
-     return savedContacts ? JSON.parse(savedContacts) : [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ];
-    }
-  );
+  //    return savedContacts ? JSON.parse(savedContacts) : [
+  //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //     ];
+  //   }
+  // );
 
-  const [filter, setFilter] = useState('');
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const addContact = ({ name, number }) => {
-    const newContact = { id: nanoid(), name, number };
-    setContacts(prevContacts => [...prevContacts, newContact]);
-  };
+  // const addContact = ({ name, number }) => {
+  //   const newContact = { id: nanoid(), name, number };
+  //   setContacts((prevContacts) => [...prevContacts, newContact]);
+  // };
 
-  const deleteContact = (contactId) => {
-    setContacts(prevContacts => 
-       prevContacts.filter((contact) => contact.id !== contactId));
-  };
+  // const deleteContact = (contactId) => {
+  //   setContacts((prevContacts) =>
+  //     prevContacts.filter((contact) => contact.id !== contactId)
+  //   );
+  // };
 
-  const handleChange = (event) => {
-    setFilter(event.target.value);
-  };
+  // const handleChange = (event) => {
+  //   setFilter(event.target.value);
+  // };
 
-  const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  // const filteredContacts = contacts.filter((contact) =>
+  //   contact.name.toLowerCase().includes(filter.toLowerCase())
+  // );
 
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
-      <SearchBox value={filter} onChange={handleChange} />
-      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+      {/* <ContactForm onSubmit={addContact} />
+      <SearchBox value={filter} onChange={handleChange} /> */}
+      <ContactList contacts={contacts} onDelete={deleteContact} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,21 @@
-import Contact from '../Contact/Contact'
-import s from './ContactList.module.css'
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+import { selectContacts } from "../../redux/contacts/selectors";
 
-const ContactList = ({contacts, onDelete}) => {
-    return (
-        <ul className={s.contactList}>{contacts.map((contact) => {
-            return (
-                <li key={contact.id} className={s.contactItem}>
-                    <Contact contactInfo={contact} onDelete={onDelete} />
-                </li>
-            );
-        })}
-        </ul>
-    );
-}
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  return (
+    <ul className={s.contactList}>
+      {contacts.map((contact) => {
+        return (
+          <li key={contact.id} className={s.contactItem}>
+            <Contact contactInfo={contact} onDelete={onDelete} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default ContactList
+export default ContactList;
