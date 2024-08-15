56e71267165e49a0ab6071bc3d545d4f45799072
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-08-15 23:03:14 +0300

    selectors

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,28 +1,28 @@
-import { useSelector } from 'react-redux';
-import { Task } from '../Task/Task';
-import { getTasks, getStatusFilter } from '../../redux/selectors';
-import { statusFilters } from '../../redux/constants';
-import css from './TaskList.module.css';
+import { useSelector } from "react-redux";
+import { Task } from "../Task/Task";
+import { selectVisibleTasks } from "../../redux/selectors";
+// import { statusFilters } from "../../redux/constants";
+import css from "./TaskList.module.css";
 
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter((task) => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter((task) => task.completed);
-    default:
-      return tasks;
-  }
-};
+// const getVisibleTasks = (tasks, statusFilter) => {
+//   switch (statusFilter) {
+//     case statusFilters.active:
+//       return tasks.filter((task) => !task.completed);
+//     case statusFilters.completed:
+//       return tasks.filter((task) => task.completed);
+//     default:
+//       return tasks;
+//   }
+// };
 
 export const TaskList = () => {
-  const tasks = useSelector(getTasks);
-  const statusFilter = useSelector(getStatusFilter);
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  const tasks = useSelector(selectVisibleTasks);
+  // const statusFilter = useSelector(selectStatusFilter);
+  // const visibleTasks = getVisibleTasks(tasks, statusFilter);
 
   return (
     <ul className={css.list}>
-      {visibleTasks.map((task) => (
+      {tasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
